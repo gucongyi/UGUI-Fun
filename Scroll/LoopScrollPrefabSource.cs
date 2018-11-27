f2680ba357d05f2f93fd20cbdef668aacc3b3c49
gucongyi <412638119@qq.com>
2018-11-27 17:00:58 +0800

    ugui func

---
+++ Scroll/LoopScrollPrefabSource.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.UI
+{
+    [System.Serializable]
+    public class LoopScrollPrefabSource
+    {
+        public GameObject templateGo;
+        public string poolName;
+        public int poolSize = 5;
+
+        private bool inited = false;
+        public virtual GameObject GetObject()
+        {
+            //var go = GameObject.Instantiate(templateGo);
+            //return go;
+            if (!inited)
+            {
+                SG.ResourceManager.Instance.InitPool(poolName, templateGo, poolSize);
+                inited = true;
+            }
+           
+            var obj = SG.ResourceManager.Instance.GetObjectFromPool(poolName, templateGo);
+            //Debug.LogError($"取一个，还剩下{SG.ResourceManager.Instance.PoolActiveCount(poolName)}");
+            return obj;
+        }
+
+        public virtual void ReturnObject(Transform go)
+        {
+           
+            go.SendMessage("ScrollCellReturn", SendMessageOptions.DontRequireReceiver);
+            SG.ResourceManager.Instance.ReturnObjectToPool(go.gameObject);
+            //Debug.LogError($"存一个，还剩下{SG.ResourceManager.Instance.PoolActiveCount(poolName)}");
+        }
+    }
+}
---
+++ Scroll/ScrollIndex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class ScrollIndex : MonoBehaviour
+{
+    public Action<int> IndexAction;
+    int index;
+
+    public void ScrollCellIndex(int idx)
+    {
+        index = idx;
+        IndexAction?.Invoke(index);
+    }
+
+
+
+}
