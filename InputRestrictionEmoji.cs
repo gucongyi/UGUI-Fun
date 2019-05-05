4140a70bcd418d1bfc6e431391d29083543c9ab7
gucongyi <412638119@qq.com>
2019-05-05 14:32:26 +0800

    输入表情报错 Error: UTF-16 to UTF-8 conversion failed because the input string is invalid

---
+++ InputRestrictionEmoji.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+public class InputRestrictionEmoji : MonoBehaviour
+{
+
+    InputField mtext;
+
+    private void Start()
+    {
+        mtext = GetComponent<InputField>();
+        if (mtext)
+        {
+            mtext.onValueChanged.AddListener(OnInputValue);
+        }
+    }
+
+    void OnInputValue(string value)
+    {
+        string msg = mtext.text;
+        //屏蔽emoji
+        string result = Regex.Replace(msg, @"\p{Cs}", "");
+        mtext.text = result;
+    }
+}
