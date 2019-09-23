548c59a1826585b09e9a6a3794af2226e43ff8e8
gucongyi <gucongyii@163.com>
2019-09-23 15:56:02 +0800

    多边形点击

---
+++ UGUIlmagePolygon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UGUIlmagePolygon : Image
+{
+    PolygonCollider2D imageCollider;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        imageCollider = GetComponent<PolygonCollider2D>();
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        var _hit = imageCollider.OverlapPoint(eventCamera.ScreenToWorldPoint(screenPoint));
+        return _hit;
+    }
+}
