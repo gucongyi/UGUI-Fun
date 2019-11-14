9fabb4f34801f9e6a201e0c3e839330ef74bea4f
gucongyi <gucongyii@163.com>
2019-11-14 16:55:38 +0800

    请立即装备<a href=mujian>木剑,我是有底线的，哈哈哈哈</a>武器
    空格要用英文空格

---
+++ TestHref.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHref : MonoBehaviour
+{
+    public LinkImageText textPic;
+
+    void Awake()
+    {
+        textPic = GetComponent<LinkImageText>();
+    }
+
+    void OnEnable()
+    {
+        textPic.onHrefClick.AddListener(OnHrefClick);
+    }
+
+    void OnDisable()
+    {
+        textPic.onHrefClick.RemoveListener(OnHrefClick);
+    }
+
+    private void OnHrefClick(string hrefName)
+    {
+        Debug.Log("点击了 " + hrefName);
+    }
+}
