4a776f5ec720bcb1dfdde7a85ec97aa1bc66af5f
gucongyi <gucongyii@163.com>
2020-06-01 20:51:57 +0800

    UGUI 世界装屏幕，屏幕转世界互转

---
+++ UGUIWorldToScreenAndReverse/Assets/Scripts/TestWorldToScreenToLocal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestWorldToScreenToLocal : MonoBehaviour
+{
+    public Canvas canvas;
+    public Image image2;
+    public Image image;
+    public Image image3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Vector2 image2Pos=RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, image2.transform.position);
+        Debug.Log($"image2.transform.position:{image2.transform.position} image2Pos:{image2Pos}");
+        Vector3 worldPoint = Vector3.zero;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(canvas.GetComponent<RectTransform>(), image2Pos, canvas.worldCamera, out worldPoint);
+        Debug.Log($"worldPoint:{worldPoint}");
+        image.transform.position = worldPoint;
+        image3.transform.position = worldPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
