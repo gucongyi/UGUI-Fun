7fd04dbe901e9b9ba3cf0e9a9491661e08dfae1b
gucongyi <gucongyii@163.com>
2021-01-29 14:02:33 +0800

    Scroll View 滑动到下一页 效果

---
+++ PageView_/PageView/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private Text pageNumber;
+    [SerializeField]
+    private InputField inputField;
+    [SerializeField]
+    private PageView pageView;
+	// Use this for initialization
+	void Start () {
+        pageNumber.text = string.Format ("当前页码：0");
+        pageView.OnPageChanged = pageChanged;
+    }
+
+    void pageChanged (int index) {
+        pageNumber.text = string.Format ("当前页码：{0}" , index.ToString ());
+    }
+
+    public void onClick () {
+        try {
+            int idnex = int.Parse (inputField.text);
+            pageView.pageTo (idnex);
+        } catch(Exception ex) {
+            Debug.LogWarning ("请输入数字"+ex.ToString()); 
+        }
+    }
+
+    void Destroy () {
+        pageView.OnPageChanged = null;
+    }
+}
